d506f76517b4a64315adcf36118160af50dbc8c0
Krunal79270 <43480840+Krunal79270@users.noreply.github.com>
2025-02-26 21:23:28 +0530

    Create index.js

---
+++ src/assets/index.js
@@ -0,0 +1,88 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import simEZ from "./simEZ.png";
+import OCL from './OCL.png'
+import Abhedya from './Abhedya.png'
+import scrap from './scrap.png'
+import Arga from './Arga.png'
+import EmVite from './EmVite.png'
+import Supi from './Supi.png'
+import elefant from './Elefant.png'
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import bootstrap from './tech/bootstrap.svg'
+import nextjs from './tech/nextjs.svg'
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+import mysql from './tech/mysql.svg'
+import graphql from './tech/graphql.svg'
+import aws from './tech/aws.svg'
+import meta from "./company/meta.png";
+import archi_city from "./archi_city.png";
+import ems from './ems.png'
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+import plutus from "./company/logo_plutus.jfif";
+import kryptoninc from "./company/logo_kryptoninc.jfif";
+import squad from "./company/logo_squad.jfif";
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  plutus,
+  kryptoninc,
+  squad,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  meta,
+  carrent,
+  jobit,
+  tripguide,
+  simEZ,
+  OCL,
+  archi_city,
+  ems,
+  Abhedya,
+  scrap,
+  Arga,
+  elefant,
+  EmVite,
+  Supi,
+  aws,
+  graphql,
+  mysql,
+  bootstrap,
+  nextjs
+};
