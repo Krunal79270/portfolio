c2385fe1c92700313ce17ebcbbda862170d4db5f
Krunal79270 <43480840+Krunal79270@users.noreply.github.com>
2025-02-27 21:58:31 +0530

    Update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -6,7 +6,7 @@ import web from "./web.png";
 import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
-import simEZ from "./simEZ.png";
+import simEZ from "./SimEZ.png";
 import OCL from './OCL.png'
 import Abhedya from './Abhedya.png'
 import scrap from './scrap.png'
